e48b69a5f18235bbc33649af17815f782966feda
nayeon0731 <toothless1596@gmail.com>
2022-05-26 16:34:11 +0900

    todo 추가

--- src/index.js
+++ src/index.js
@@ -1,9 +1,29 @@
 import { log } from './utils';
 import './todos.css';
 
+const todos = []
+
 //폼, input 입력 이벤트 추가하기
 //입력 시 todo추가
 const $form = document.querySelector('.new-task');
-$form.addEventListener('submit', ()=>{})
+const $input = document.querySelector('.new-task > input');
 
-//todos뿌려주기
+const checkTodo = (event) => {
+    event.preventDefault();
+
+    console.log($input.value)
+
+    //todo추가
+    const todo = {title: $input.value, isDone: false};
+    todos.push(todo);
+    //todos뿌려주기
+
+//input 초기화
+
+// todos출력
+}
+
+$form.addEventListener('submit', checkTodo)
+
+
+
