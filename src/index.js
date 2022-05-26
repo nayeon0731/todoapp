6e06d78841d4d1c75338cd03f098bbffa5e66478
nayeon0731 <toothless1596@gmail.com>
2022-05-26 16:40:21 +0900

    todo 출력

--- src/index.js
+++ src/index.js
@@ -1,13 +1,25 @@
 import { log } from './utils';
 import './todos.css';
 
-const todos = []
+const todos = [
+    {title: "HTML", isDone: true},
+    {title: "JS", isDone: true},
+    {title: "React", isDone: true}
+]
 
 //폼, input 입력 이벤트 추가하기
 //입력 시 todo추가
 const $form = document.querySelector('.new-task');
 const $input = document.querySelector('.new-task > input');
+const $todos = document.querySelector('#todos');
 
+const printTodos = () => {
+    const html = todos.map((todo) => {
+        return '<li>' + todo.title + '</li>'
+    });
+
+    $todos.innerHTML = `<ul>${html.join('')}</ul>`
+}
 const checkTodo = (event) => {
     event.preventDefault();
 
@@ -18,9 +30,13 @@ const checkTodo = (event) => {
     todos.push(todo);
     //todos뿌려주기
 
-//input 초기화
+    //input 초기화
+    $input.value = ""
+
+    printTodos();
 
 // todos출력
+
 }
 
 $form.addEventListener('submit', checkTodo)
