1ac718780280f0e11e34a7ab0bf0556d135c55b4
nayeon0731 <toothless1596@gmail.com>
2022-05-26 17:00:03 +0900

    삭제 구현

--- src/index.js
+++ src/index.js
@@ -7,39 +7,66 @@ const todos = [
     {title: "React", isDone: true}
 ]
 
-//폼, input 입력 이벤트 추가하기
-//입력 시 todo추가
 const $form = document.querySelector('.new-task');
 const $input = document.querySelector('.new-task > input');
 const $todos = document.querySelector('#todos');
 
 const printTodos = () => {
-    const html = todos.map((todo) => {
-        return '<li>' + todo.title + '</li>'
+    const html = todos.map((todo, index) => {
+        return `
+        <li data-index = "${index}">
+        <button class="delete">×</button>
+        <input type="checkbox" class="toggle-checked" />
+        <span class="text">${todo.title}</span>
+        </li>
+        `
     });
 
     $todos.innerHTML = `<ul>${html.join('')}</ul>`
-}
+};
+
 const checkTodo = (event) => {
     event.preventDefault();
-
-    console.log($input.value)
+    console.log($input.value);
 
     //todo추가
     const todo = {title: $input.value, isDone: false};
     todos.push(todo);
-    //todos뿌려주기
+    console.log(todos);
 
     //input 초기화
     $input.value = ""
-
+    
+    //todos출력
     printTodos();
-
-// todos출력
-
 }
 
-$form.addEventListener('submit', checkTodo)
+// 폼,input 입력 이벤트 추가하기
+// 입력했을때 todo추가
+$form.addEventListener('submit', checkTodo);
+
+const deleteTodo=(index) => {
+    console.log('delete', index);
+    //todos에서 index번째 todo삭제
+    todos.splice(index,1)
+    printTodos()
+};
+
+const toggleTodo=()=> {
+    console.log('toggle');
+};
+
+//삭제
+document.body.addEventListener('click', (event)=> {
+    const index = parseInt(event.target.parentNode.dataset.index, 10);
+    if(console.log(event.target.className === 'delete')) {
+        deleteTodo(index)
+    } else if(event.target.className==="toggle-checked") {
+        toggleTodo(index);
+    }
+})
+
+printTodos();
 
 
 
